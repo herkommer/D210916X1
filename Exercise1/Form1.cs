e1aae4160bcf1294886a12f4d2740364207aca2f
herkommer <m@herkommer.co.uk>
2021-09-16 11:07:23 +0200

    inital commit

---
+++ Exercise1/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercise1
+{
+    public partial class Form1 : Form
+    {
+        //Constructor
+        //dvs, denna kod körs vid NEW
+        public Form1()
+        {
+            //Denna metod, kommer nu se till att 
+            //köra kod för att skapa alla UI komponenter
+            InitializeComponent();
+
+            //Nu är vårt formulär klart och redo att användas
+            button1.Text = "Click me!";
+        }
+
+        private void button1_Clickk(object sender, EventArgs e)
+        {
+            //MessageBox.Show("Vem var det som kasta! " + sender.GetType());
+            textBox1.Text = "Kilroy was here";
+        }
+
+        private void Klicker(object sender, EventArgs e)
+        {
+            textBox1.Text = "Kilroy was here";
+        }
+    }
+}
---
+++ Exercise2/Welcome.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercise2
+{
+    public partial class Welcome : Form
+    {
+        public Welcome()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int i, j;
+            i = int.Parse(textBox1.Text);   //C#
+            j = Int32.Parse(textBox2.Text); //Common Type System
+
+            //MessageBox.Show($"{i} + {j} = {i+j}");    //Modal
+            listBox1.Items.Add($"{i} + {j} = {i + j}");
+        }
+    }
+}
